5a9c615abe68761553375b5c892f900ba9f2597a
RajibTheKing <aporba.das@gmail.com>
2025-04-30 16:07:48 +0200

    remove some trailing spaces

--- TrainClient/NetworkWorker.py
+++ TrainClient/NetworkWorker.py
@@ -7,17 +7,17 @@ from PyQt5.QtCore import QTimer, Qt, QThread, pyqtSignal, pyqtSlot
 
 class NetworkWorker(QThread):
     packet_sent = pyqtSignal(int)  # Signal to emit when packet is sent
-    
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.packet_queue = queue.Queue()
         self.running = True
-        
+
     def run(self):
         context = zmq.Context()
         socket = context.socket(zmq.PUB)
         socket.bind("tcp://*:5555")
-        
+
         while self.running:
             try:
                 packet = self.packet_queue.get(timeout=0.1)
@@ -28,13 +28,13 @@ class NetworkWorker(QThread):
                     self.packet_sent.emit(len(packet))
             except queue.Empty:
                 continue
-                
+
         socket.close()
         context.term()
-        
+
     def stop(self):
         self.running = False
         self.wait()
-        
+
     def enqueue_packet(self, packet):
         self.packet_queue.put(packet)
