674cdb8682e33139dea4af59adc088ec2d190d12
RajibTheKing <aporba.das@gmail.com>
2025-06-16 11:52:04 +0200

    Add remote droplet IP for CORS origins

--- central-server/src/config.py
+++ central-server/src/config.py
@@ -1,7 +1,9 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    CORS_ORIGINS: list = ["http://localhost:8080"]  # Vue.js client
+    CORS_ORIGINS: list = [
+        "http://localhost:8080",
+        "http://209.38.218.207:8080"]  # Vue.js client
     MAX_WS_MESSAGE_SIZE: int = 100 * 1024 * 1024  # 100MB for video frames
 
 settings = Settings()
