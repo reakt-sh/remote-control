752bdc86b0f77f530e695255d755cbce66ca63c3
RajibTheKing <aporba.das@gmail.com>
2025-06-16 12:42:53 +0200

    Fix a bug in CORS origin

--- central-server/src/config.py
+++ central-server/src/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     CORS_ORIGINS: list = [
         "http://localhost:8080",
-        "http://209.38.218.207:8080"]  # Vue.js client
+        "https://209.38.218.207:8080"]
     MAX_WS_MESSAGE_SIZE: int = 100 * 1024 * 1024  # 100MB for video frames
 
 settings = Settings()
