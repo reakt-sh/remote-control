7cc26b54e973475611e4deb31825b8e06984c22c
RajibTheKing <aporba.das@gmail.com>
2025-06-16 14:06:37 +0200

    Update WebSocket connection and make it secured for train client

--- web-client/vue.config.js
+++ web-client/vue.config.js
@@ -5,10 +5,13 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.BASE_URL || '/',
   devServer: {
-    port: 8080, // or your desired port
-    https: {
-      key: fs.readFileSync('/etc/ssl/quic_conf/certificate.key'),
-      cert: fs.readFileSync('/etc/ssl/quic_conf/certificate.pem'),
-    },
+    port: 8080,
+    server: {
+      type: 'https',
+      options: {
+        key: fs.readFileSync('/etc/ssl/quic_conf/certificate.key'),
+        cert: fs.readFileSync('/etc/ssl/quic_conf/certificate.pem'),
+      }
+    }
   }
 });
