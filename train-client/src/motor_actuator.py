4102146b57fea94bdf76e2e11328f087b5ddad12
RajibTheKing <aporba.das@gmail.com>
2025-06-25 14:35:25 +0200

    Update motor actuator logic

--- train-client/src/motor_actuator.py
+++ train-client/src/motor_actuator.py
@@ -7,6 +7,7 @@ class MotorActuator:
         self.enable_pin = enable_pin    # GPIO pin for EN, used for PWM control
         self.pwm_freq = pwm_freq        # Frequency for PWM control
         self.direction = 1              # 1 for forward, 0 for backward
+        self.speed = 25                 # Default speed (duty cycle for P
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.input1_pin, GPIO.OUT)
@@ -15,32 +16,37 @@ class MotorActuator:
         GPIO.output(self.input1_pin, GPIO.LOW)
         GPIO.output(self.input2_pin, GPIO.LOW)
         self.pwm = GPIO.PWM(self.enable_pin, self.pwm_freq)
-        self.pwm.start(25)  # Default to low speed
+        self.pwm.start(self.speed)  # Default to low speed
 
     def start_motor(self):
+        self.speed = max(self.speed, 25) # Ensure minimum speed
+        self.set_speed(self.speed)
         if self.direction == 1:
-            self.move_forward()
+            GPIO.output(self.input1_pin, GPIO.HIGH)
+            GPIO.output(self.input2_pin, GPIO.LOW)
         else:
-            self.move_backward()
+            GPIO.output(self.input1_pin, GPIO.LOW)
+            GPIO.output(self.input2_pin, GPIO.HIGH)
 
     def stop_motor(self):
         GPIO.output(self.input1_pin, GPIO.LOW)
         GPIO.output(self.input2_pin, GPIO.LOW)
 
     def move_forward(self):
-        GPIO.output(self.input1_pin, GPIO.HIGH)
-        GPIO.output(self.input2_pin, GPIO.LOW)
         self.direction = 1
+        self.start_motor()
 
     def move_backward(self):
-        GPIO.output(self.input1_pin, GPIO.LOW)
-        GPIO.output(self.input2_pin, GPIO.HIGH)
         self.direction = 0
+        self.start_motor()
 
     def set_speed(self, speed):
-        speed = int(speed * 1.25) # Scale speed to match PWM range
-        speed = max(0, min(75, speed))
-        self.pwm.ChangeDutyCycle(speed)
+        self.speed = int(speed * 1.25) # Scale speed to match PWM range
+        self.speed = max(0, min(75, speed))
+        self.pwm.ChangeDutyCycle(self.speed)
+
+    def get_speed(self):
+        return int(self.speed / 1.25)  # Convert back to original speed scale
 
     def cleanup(self):
         self.stop()
