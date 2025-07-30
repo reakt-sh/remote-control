ea40222e9739871a870e0fc1405c8509229b6b8c
RajibTheKing <aporba.das@gmail.com>
2025-07-30 14:32:15 +0200

    Update nanomq config

--- web-client/src/scripts/config.js
+++ web-client/src/scripts/config.js
@@ -5,5 +5,5 @@ const MQTT_WS_PORT = 8083
 const SERVER_URL = `https://${SERVER}:${WS_PORT}`
 const WS_URL = `wss://${SERVER}:${WS_PORT}`
 const QUIC_URL = `https://${SERVER}:${QUIC_PORT}`
-const MQTT_BROKER_URL = `ws://${SERVER}:${MQTT_WS_PORT}/mqtt`
+const MQTT_BROKER_URL = `wss://${SERVER}:${MQTT_WS_PORT}/mqtt`
 export { SERVER_URL, WS_URL, QUIC_URL, MQTT_BROKER_URL }
