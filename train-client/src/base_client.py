e663622fd6193152ae7587aa10d3063d9b477bf2
RajibTheKing <aporba.das@gmail.com>
2025-10-28 14:20:03 +0100

    Update telemetry sending logic, now using only MQTT

--- train-client/src/base_client.py
+++ train-client/src/base_client.py
@@ -218,8 +218,8 @@ class BaseClient(ABC, metaclass=QABCMeta):
             packet_data = json.dumps(data).encode('utf-8')
             packet = struct.pack("B", PACKET_TYPE["telemetry"]) + packet_data
             # Send telemetry on all active connections
-            self.network_worker_quic.enqueue_stream_packet(packet)
-            self.network_worker_ws.enqueue_packet(packet)
+            # self.network_worker_quic.enqueue_stream_packet(packet)
+            # self.network_worker_ws.enqueue_packet(packet)
             self.network_worker_mqtt.send_data(packet_data)
 
     def on_imu_data(self, data):
