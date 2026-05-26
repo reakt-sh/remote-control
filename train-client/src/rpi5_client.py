6c6e5798d82dcc2e83951a8956f203f9ef0fb077
RajibTheKing <aporba.das@gmail.com>
2026-05-26 14:22:09 +0200

    Add new method

--- train-client/src/rpi5_client.py
+++ train-client/src/rpi5_client.py
@@ -35,3 +35,19 @@ class RPi5Client(BaseClient, QThread):
         elif direction == DIRECTION["BACKWARD"]:
             self.motor_actuator.move_backward()
         self.telemetry.set_status(TRAIN_STATUS["POWER_ON"])
+
+    def on_headlight_on(self):
+        logger.info("Turning on headlights.")
+        self.motor_actuator.set_led_turn_on()
+    
+    def on_headlight_off(self):
+        logger.info("Turning off headlights.")
+        self.motor_actuator.set_led_turn_on()  # Assuming the same method toggles the LED state
+
+    def on_horn_on(self):
+        logger.info("Horn on - not implemented yet.")
+        # Implement horn control if hardware supports it
+    
+    def on_horn_off(self):
+        logger.info("Horn off - not implemented yet.")
+        # Implement horn control if hardware supports it
